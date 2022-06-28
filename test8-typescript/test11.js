7eb88bded69662368ea38fe2edecd645a8d4be16
echolu <526710073@qq.com>
2022-06-28 15:59:43 +0800

    数字枚举类型 字符串枚举类型

---
+++ test8-typescript/test11.js
@@ -0,0 +1,30 @@
+//枚举
+//数字枚举
+//  enum Direction  {
+//         Up = 1,
+//         Down,
+//         Left,
+//         Right
+//  }
+//  console.log(Direction.Down)
+//  enum E {
+//     D,
+//     A = getSomeValue(20),   //枚举成员必须具有初始化表达式，第一个枚举参数不能用函数获取值,只能放在初始化的束纸枚举之后
+//     B = 10
+//  }
+//  function getSomeValue(n: number): number {
+//     return n
+//  }
+//   console.log(E.A)
+//   console.log(E.D)
+//字符串枚举
+//每个成员必须是字符串，或另一个枚举型字符串成员进行初始化
+//它不像数字枚举类型，没有自增长
+var BooleanLikeogeneousEnum;
+(function (BooleanLikeogeneousEnum) {
+    BooleanLikeogeneousEnum["Up"] = "Up";
+    BooleanLikeogeneousEnum["Dowm"] = "Dowm";
+    BooleanLikeogeneousEnum["Left"] = "Left";
+    BooleanLikeogeneousEnum["Right"] = "Right";
+})(BooleanLikeogeneousEnum || (BooleanLikeogeneousEnum = {}));
+console.log(BooleanLikeogeneousEnum.Left);
